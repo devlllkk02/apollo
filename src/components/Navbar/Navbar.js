079d49f1015609909beb3a717285e45d283a014f
Naveen Liyanage <naveenlllkk02@gmail.com>
2021-10-29 20:17:35 +0530

    Changed major files and initialized the project

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Navbar.scss";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <img src="./images/Apollo..png" alt="we" />
+    </div>
+  );
+}
+
+export default Navbar;
